6f91b8755e8d6e3acfa12af0675123a8091415e6
martin <eGoicoechea>
2019-05-04 03:29:51 -0300

    Git ignore updated and Tweet works!

--- Twit.js
+++ Twit.js
@@ -1,17 +1,16 @@
 var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key : '...',
-  consumer_secret: '...',
-  access_token: '...',
-  access_token_secret: '...',
-  timeout_ms: 60*1000,
-  strictSSL:   true,
-  })
+var config = require('./twitterConfig.json');
+
+console.log(config);
+
+var T = new Twit(config);
+
+
   
   //hacemos el tweet 
   
- T.post('statuses/update', { status: 'seleccionamos las mujeres  mas influyentes en Ciencias en Mayo. Pegate la vuelta!' }, function(err, data, response) {
+ T.post('statuses/update', { status: 'test las mujeres  mas influyentes en Ciencias en Mayo. Pegate la vuelta!' }, function(err, data, response) {
   console.log(data)
 })
   
