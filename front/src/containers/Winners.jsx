382ff33b154b6cc50f2ab65819590bc159087a48
martin <martinsilva.1995@gmail.com>
2019-05-04 11:33:00 -0300

    last

--- front/src/containers/Winners.jsx
+++ front/src/containers/Winners.jsx
@@ -24,15 +24,16 @@ class AdminView extends React.Component {
             {
                 this.props.winners.map( elemento => 
             <div class="card">
-              <img class="card-img-top rounded-circle" src="img1.jpg" alt="Card image cap"></img>
+              <img class="card-img-top rounded-circle" src="http://blogbibliotecas.mecd.gob.es/wp-content/uploads/2017/01/Imagen-destacada-Mujeres-y-ciencia1-e1484738511133.jpg" alt="Card image cap"></img>
               <div class="card-body">
-                <h5 class="card-title">{elemento.nombre}+ +{elemento.apellido} }</h5>
-                <p class="card-text">Edad: {elemento.edad}}</p>
-                <p class="card-text">Area: {elemento.campo}}</p>
+                <h5 class="card-title">{elemento.nombre} {elemento.apellido} </h5>
+                <p class="card-text">Edad: {elemento.edad}</p>
+                <p class="card-text">Area: {elemento.campo}</p>
                 <a href = "https://www.facebook.com" class="fa fa-facebook"></a>
                  <a href="https://www.twitter.com" class="fa fa-twitter"></a>
                 <a href="https://www.gmail.com" class = "fa fa-google"></a>
                 <a href="https://www.linkedin.com" class="fa fa-linkedin"></a>
+                
               </div>
             </div>
                 )
