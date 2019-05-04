5b98f98db7c570be6dd9387aff9016707f77f561
martin <martinsilva.1995@gmail.com>
2019-05-04 01:24:46 -0300

    update apjs

--- back/app.js
+++ back/app.js
@@ -38,11 +38,11 @@ app.post('/api/savePostulado', function(req, res) {
 */
 
   if(insertPostulado(req.body)){
-    res.send(200)
+    res.send("ok")
   }
   
   else{
-    res.send(403)
+    res.send("wrong")
   }
 
 
@@ -105,6 +105,6 @@ sequelize.authenticate()
 
 
 
-server.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+server.listen(3001, function() {
+  console.log("Node server running on http://localhost:3001");
 });
