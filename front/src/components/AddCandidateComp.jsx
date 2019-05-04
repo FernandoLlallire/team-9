e8587a2f54fc5cd882fea915383fa7fa7cd2d309
fernandollallire <fernandorllallire@gmail.com>
2019-05-04 03:10:23 -0300

    .git ignore

--- front/src/components/AddCandidateComp.jsx
+++ front/src/components/AddCandidateComp.jsx
@@ -39,51 +39,41 @@ const candidate = (props) => {
               <input onChange={props.onChange} type="text" className="form-control inputLogin" id="edad" placeholder="* Edad" name='edad' />
             </div>
             <div className="form-group">
-              <label htmlFor="email">* Provincia</label>
-              <input onChange={props.onChange} type="text" className="form-control inputLogin" id="provincia" placeholder="provincia" name='provincia' />
+              <input onChange={props.onChange} type="text" className="form-control inputLogin" id="provincia" placeholder="* Provincia" name='provincia' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* e-mail</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="email" placeholder="e-mail" name='email' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="email" placeholder="* E-mail" name='email' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Teléfono</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="telefono" placeholder="Mobile Number" name='telefono' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="telefono" placeholder="* Teléfono" name='telefono' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Rubro de ciencia/tecnología</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="campo" placeholder="Mobile Number" name='campo' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="campo" placeholder="* Rubro de ciencia/tecnología" name='campo' />
             </div>
             <div className="form-group">
               <label htmlFor="workExperince">Extracto</label>
               <textarea onChange={props.onChange} className="form-control inputLogin" id="WorkExperience" rows="3" name="extracto"></textarea>
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Teléfono aplicante</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="telefono_tercero" placeholder="telefono tercero" name='telefono_tercero' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="telefono_tercero" placeholder="* Teléfono aplicante" name='telefono_tercero' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* e-mail aplicante</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="mail_tercero" placeholder="emailTercero" name='mail_tercero' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="mail_tercero" placeholder="* E-mail aplicante" name='mail_tercero' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Motivo</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="motivo" placeholder="motivo" name='motivo_post' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="motivo" placeholder="* Motivo" name='motivo_post' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Foto</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="foto" placeholder="foto" name='foto' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="foto" placeholder="* Foto" name='foto' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Video</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="video" placeholder="video" name='video' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="video" placeholder="* Video" name='video' />
             </div>
             <div className="form-group">
-              <label htmlFor="number">* Audio</label>
-              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="audio" placeholder="audio" name='audio' />
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="audio" placeholder="* Audio" name='audio' />
             </div>
             <div>
-              <button onSubmit={props.onSubmit} type="submit" className="btn boton btn-lg botonLogin btn-primary" data-toggle="modal" data-target="#exampleModal">Add Applicant</button>
+              <button onSubmit={props.onSubmit} type="submit" className="btn boton btn-lg botonLogin btn-primary align-self-center" data-toggle="modal" data-target="#exampleModal">Add Applicant</button>
               {/* <button onClick={() => props.onClick()} className="btn boton btn-lg botonLogin btn-primary" >View All Candidates</button> */}
             </div>
           </form>
--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,50 +1,55 @@
 import React from 'react';
 
 const navBar = (props) => {
-  return (<div class="">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="divImgLogo">
-        <a class="navbar-brand" href="https://www.chicasentecnologia.org/"><img class="imgLogo" src="logo.webp" alt=""></img></a>
+  return (<div className="">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="divImgLogo">
+        <a className="navbar-brand" href="https://www.chicasentecnologia.org/"><img className="imgLogo" src="logo.webp" alt=""></img></a>
       </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto listMargin">
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.chicasentecnologia.org/">Inicio <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto listMargin">
+          <li className="nav-item">
+            <a className="nav-link" href="https://www.chicasentecnologia.org/">Inicio <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Nosotrxs
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/sobre">Sobre</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/prensa">Prensa</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown1">
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/sobre">Sobre</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/prensa">Prensa</a>
             </div>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Qué hacemos
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/programando-un-mundo-mejor">Programando un Mundo Mejor</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/clubes">Clubes</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/comunidad">Comunidad</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/investigacion">Investigacion</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/talleres-charlas-y-eventos">Talleres, charlas y eventos</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/campanas">Campañas</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown2">
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/programando-un-mundo-mejor">Programando un Mundo Mejor</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/clubes">Clubes</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/comunidad">Comunidad</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/investigacion">Investigacion</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/talleres-charlas-y-eventos">Talleres, charlas y eventos</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/campanas">Campañas</a>
             </div>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Socios
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/nuestros-socios">Nuestros socios</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/reportes">Reportes</a>
-              <a class="dropdown-item" href="https://www.chicasentecnologia.org/involucrate">Involucrarte</a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown3">
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/nuestros-socios">Nuestros socios</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/reportes">Reportes</a>
+              <a className="dropdown-item" href="https://www.chicasentecnologia.org/involucrate">Involucrarte</a>
             </div>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Agregar Candidatos
+            </a>
           </li>
         </ul>
       </div>
