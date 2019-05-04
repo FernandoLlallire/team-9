4396afd703f61f9ca20405fac1cafacfef602bb6
Juan Manuel Ibañez <jmiban@gmail.com>
2019-05-03 23:46:08 -0300

    formulario

---
+++ front/src/components/AddCandidateComp.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const candidate = (props) => {
+  return (
+    <div>
+      <div>
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <p>{props.message && props.message}</p>
+              </div>
+              <div className="modal-footer">
+                {/* { */}
+                 <button type="button" onClick={() => props.onClick()} className="btn btn-lg" style={{ backgroundColor: '#DE411B' }} data-dismiss="modal" > { props.message === 'Successfully saved Candidate' ? 'Go to Candidates' : 'Go back' } </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="form-group form-control-lg row formUser" style={{ marginTop: '50px', padding: '20px' }}>
+        <div className='col-lg-3'></div>
+        <div className='col-lg-6 addUser'>
+          <form onSubmit={props.onSubmit} className='formUser' >
+            <h1 className='titHomeForms'>Agregar nueva candidata</h1>
+            <small>(<b>*</b>) Campos requeridos</small>
+            <p/>
+            <div className="form-group " >
+              <label htmlFor="lastName">* Nombre</label>
+              <input onChange={props.onChange} type="text" className="form-control inputLogin" id="LastName" placeholder="Name" name='name' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="firsstName">* Apellido</label>
+              <input onChange={props.onChange} type="text" className="form-control inputLogin" id="First Name" placeholder="Last Name" name='surname' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">* Edad</label>
+              <input onChange={props.onChange} type="text" className="form-control inputLogin" id="Email" placeholder="Email" name='email' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="number">* e-mail</label>
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="phone" placeholder="Mobile Number" name='telNumber' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="number">* Teléfono</label>
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="phone" placeholder="Mobile Number" name='telNumber' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="number">* Rubro de ciencia/tecnología</label>
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="phone" placeholder="Mobile Number" name='telNumber' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="workExperince">Extracto</label>
+              <textarea onChange={props.onChange} className="form-control inputLogin" id="WorkExperience" rows="3" name="expertise"></textarea>
+            </div>
+            <div className="form-group">
+              <label htmlFor="number">* Teléfono aplicante</label>
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="phone" placeholder="Mobile Number" name='telNumber' />
+            </div>
+            <div className="form-group">
+              <label htmlFor="number">* e-mail aplicante</label>
+              <input onChange={props.onChange} type="tel" className="form-control inputLogin" id="phone" placeholder="Mobile Number" name='telNumber' />
+            </div>
+            <div>
+              <button onSubmit={props.onSubmit} type="submit" className="btn boton btn-lg botonLogin btn-primary" data-toggle="modal" data-target="#exampleModal">Add Applicant</button>
+              {/* <button onClick={() => props.onClick()} className="btn boton btn-lg botonLogin btn-primary" >View All Candidates</button> */}
+            </div>
+          </form>
+
+        </div>
+        <div className='col-lg-3'></div>
+      </div>
+    </div>
+  );
+};
+
+export default candidate;
